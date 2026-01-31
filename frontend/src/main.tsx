cce46b7e76434059146fe4b3383aa9b56e0a05a9
shlokshrestha <shlokstha99@gmail.com>
2026-01-31 21:46:15 +0545

    feat: added react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { RouterProvider } from "react-router/dom";
+import router from "./router/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-     <ChakraProvider value={defaultSystem}>
-      <App />
+    <ChakraProvider value={defaultSystem}>
+      <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>,
 );
