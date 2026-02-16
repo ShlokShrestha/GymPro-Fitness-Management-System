bcf47b97ed6f7e853a8a648a5b945cf0e6a33821
shlokshrestha <shlokstha99@gmail.com>
2026-02-16 21:44:04 +0545

    feat: added client list

--- frontend/src/pages/admin/Clients/Clients.tsx
+++ frontend/src/pages/admin/Clients/Clients.tsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import DataTable from "../../../components/resuable/DataTable";
+import API from "../../../api/axios";
 
-type Props = {}
+const Clients = () => {
+  const navigate = useNavigate();
 
-const Clients = (props: Props) => {
+  const [clients, setClients] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
+
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetchClients();
+  }, [page, pageSize, search]);
+
+  const fetchClients = async () => {
+    try {
+      setLoading(true);
+      const { data } = await API.get("/user", {
+        params: {
+          page,
+          limit: pageSize,
+          search,
+        },
+      });
+      setClients(data.data);
+      setTotalItems(data.pagination.totalRecords);
+      setTotalPages(Math.ceil(data.pagination.totalRecords / pageSize));
+    } catch (error) {
+      console.error("Error fetching clients:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async (id: string) => {
+    try {
+      await API.delete(`/user/${id}`);
+      fetchClients();
+    } catch (error) {
+      console.error("Delete failed:", error);
+    }
+  };
+
+  const columns = [
+    { header: "Full Name", accessor: "fullName", sortable: true },
+    { header: "Email", accessor: "email", sortable: true },
+  ];
+
   return (
-    <div>Clients</div>
-  )
-}
+    <>
+      <div className="header">
+        <h2>Membership Clients</h2>
 
-export default Clients
+        <button
+          className="add-btn"
+          onClick={() => navigate("/admin/clients/add")}
+        >
+          + Add Client
+        </button>
+      </div>
+
+      <DataTable
+        columns={columns}
+        data={clients}
+        loading={loading}
+        searchable
+        pagination={{
+          currentPage: page,
+          totalPages,
+          totalItems,
+          pageSize,
+          onPageChange: setPage,
+          onPageSizeChange: (size) => {
+            setPageSize(size);
+            setPage(0);
+          },
+        }}
+        onSearch={(value) => {
+          setSearch(value);
+          setPage(0);
+        }}
+        onEdit={(plan) => navigate(`/admin/clients/edit/${plan.id}`)}
+        onDelete={handleDelete}
+      />
+    </>
+  );
+};
+
+export default Clients;
